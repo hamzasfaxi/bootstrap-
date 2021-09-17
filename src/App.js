eb4154bc96756a47d0a07ec42ee32fe39e8e6ff7
hamzasfaxi <88566062+hamzasfaxi@users.noreply.github.com>
2021-09-17 22:28:43 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1 className="title">welcome to the official site of Hogwarts : school of witch craft and wizardery</h1>
+         
+         
+         
+          <h3 className = 'link'>
+            let the magic begin
+          </h3>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
